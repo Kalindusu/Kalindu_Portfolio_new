62110407951a6d99ffc6158d195f882d7f879a6f
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-08 01:26:16 +0530

    index update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,4 @@
-import project1a from "../assets/projects/c1.png";
-import project1b from "../assets/projects/c2.png";
-import project1c from "../assets/projects/c3.png";
-import project1d from "../assets/projects/c4.png"
 
-import project2a from "../assets/projects/ch1.png"
-import project2b from "../assets/projects/ch2.png"
-import project2c from "../assets/projects/ch3.png"
-import project2d from "../assets/projects/ch4.png"
 
 
 
@@ -16,41 +8,4 @@ export const ABOUT_TEXT = `I'm a dedicated and versatile full stack developer pa
 
 `;
 
-export const PROJECTS = [
-  {
-    title: "Real Time Chat App",
-    images: [project2b,project2c,project2a,project2d], // Updated to include multiple images
-    description: `
-Developed a comprehensive and feature-rich web application utilizing modern technologies. The application includes:
 
-Tech Stack: MERN (MongoDB, Express, React, Node.js), Socket.io, TailwindCSS, and DaisyUI.
-Authentication & Authorization: Implemented secure authentication and authorization using JWT.
-Real-Time Messaging: Enabled seamless communication with Socket.io.
-Online User Status: Implemented dynamic user presence updates with Socket.io and React Context.
-Global State Management: Utilized Zustand for efficient state handling.
-Robust Error Handling: Implemented comprehensive error management on both server and client sides.
-Professional Deployment: Successfully deployed the application at zero cost
-    `,
-    technologies: ["MERN", "MongoDB", "React", "Node.js", "Socket.io", "TailwindCSS", "DaisyUI"],
-    links: [
-      { url: "https://chatapp-dhsa.onrender.com/",github:"https://github.com/vikasyadavvvv/Mern-ChatApp.git" }
-    ]
-  },
-  {
-    title: "Crypto Curency Tracker",
-    images: [project1a, project1b ,project1c,project1d], // Updated to include multiple images
-    description: `Developed a comprehensive cryptocurrency tracker application using React, CSS, and the CoinGecko API. Cryptotracker provides updated information on cryptocurrency prices, market trends, and market capitalization.Access to cryptocurrency prices, market capitalization, and 24-hour changes.Comprehensive coverage of both leading and emerging cryptocurrencies.Clean and intuitive design for easy navigation.
-Responsiv interface ensuring a seamless user experience across all devices.
-
-    `,
-    technologies: ["React", "CSS", "CoinGeko API"],
-    links: [
-      { url: "https://cryppto-tracker.netlify.app/" , github:"https://github.com/vikasyadavvvv/Crypto-Tracker.git" }
-    ]
-  },
-];
-
-export const CONTACT = {
-  phoneNo: "+919082539010",
-  email: "vy532555@gmail.com",
-};
--- src/data/index.jsx
+++ src/data/index.jsx
@@ -1,63 +1,62 @@
 export const project = [
-    {
-      name: "Hotel Management & Booking System",
-      details:
-        " Developed a Next.js application using Redux Toolkit for state management, React Hook Form for form handling, and Axios for API interactions. The platform features a userauthentication system allowing users to log in, view their own and others' posts, andmanage posts and comments. Implemented robust functionalities for post creation,deletion, and commenting.",
-      skills:
-        "NextJs - Typescript - Reduxjs/toolkit- TaillwindCSS - React-hook-form - Axios - headlessui ",
-      demo: "****Coming Soon****",
-      repo: "https://github.com/Kalindusu/Hotel_Management_System",
-    },
-    {
-      name: "PitCrew Mobile Application Group Project",
-      details:
-        " Is a full e-commerce web application using React.js.This project’s state management was achieved using only ContextAPI to leverage its potentials to the max. A real-world payment gateway (Stripe) was implemented in this project.",
-      skills:
-        "ReactJs - TaillwindCSS- AOS Library - Axios - React Query - React Slick",
-      demo: "https://fresh-cart-seven-gilt.vercel.app/",
-      repo: "https://github.com/ChethanaWijesinghe10/PitCrew",
-    },
-    {
-      name: "FoodOnWay E-commerce Website",
-      details:
-        "Programming Ongoing charity for all dead Muslims. The site is about the Noble Qur’an, interpretation of the Qur’an, hadiths, morning and evening remembrances, and praises.",
-      skills:
-        "React Js - Typescript - ReactQuery- TaillwindCSS - Axios - headlessui-React Hot Toast ",
-      demo: "https://quran-kareem-eta.vercel.app/",
-      repo: "https://github.com/Kalindusu/Foodie-Fusion",
-    },
-    {
-      name: "Chatbot",
-      details:
-        " I've developed a lightweight code editor similar to Visual Studio Code using React,TypeScript, Tailwind CSS, and Redux. The project features local data management withcustom page data and employs UUID for unique ID generation, ensuring robust andorganized state handling..",
-      skills:
-        "ReactJs - Typescript - Reduxjs/toolkit- TaillwindCSS- UUID - React-resizable-panels - React-syntax-highlighter",
-      demo: "https://visual-studio-code-clone-kappa.vercel.app/",
-      repo: "https://github.com/Kalindusu/ChatBot",
-    },
-    {
-      name: "CeylonTours E-Booking Mobile Application",
-      details:
-        " This React and TypeScript project is designed for managing products, featuring functionality to add items with attributes like price, category, image, colors, and description. It uses Headless UI for accessible components, TailwindCSS for modern styling, and UUID for generating unique product identifiers.",
-      skills: "React - TaillwindCSS - React HOOKS - TypeScript - Headlessui",
-      demo: "https://mohammedrady59.github.io/CRUD-SYSTEM-TS/",
-      repo: "https://github.com/Kalindusu/CeylonTours-E-booking",
-    },
-    {
-      name: "Portfolio Website",
-      details:
-        " Developed the company website front-end and currently maintaining it.",
-      skills: "ReactJs - TaillwindCSS - React Slick - AOS Library",
-      demo: "https://foanztopkw.com/",
-      repo: "https://github.com/Kalindusu/Kalindu_Portfolio_new",
-    },
-    // {
-    //   name: "Palestine",
-    //   details:
-    //     "We need to support our brothers in Palestine. By introducing their case against the Zionist occupati...",
-    //   skills: "HTML - CSS - Vanilla JS ",
-    //   demo: "https://mohammedrady59.github.io/Palestine/",
-    //   repo: "https://github.com/MohammedRady59/Palestine",
-    // },
-  ];
-  
+  {
+    name: "Hotel Management & Booking System",
+    details:
+      " Currently developing a comprehensive hotel booking system using Spring Boot, MySQL, and ReactJS. The system is designed to provide a secure, scalable backend with robust authentication and data management. The user-friendly frontend offers a seamless booking experience for customers, including room selection, availability checking, and payment processing. The system also features an intuitive admin dashboard for managing reservations, customer data, room availability, and analytics, empowering hotel administrators to make informed decisions and optimize operations. Integration with third-party APIs for payments and real-time notifications ensures a modern, professional experience.",
+    skills:
+      "Spring Boot - Spring Security - MySQL - JWT - Hibernate - ReactJS - Axios - TailwindCSS - React Router - AWS S3",  
+    demo: "****Coming Soon****",
+    repo: "https://github.com/Kalindusu/Hotel_Management_System",
+  },
+  {
+    name: "PitCrew Mobile Application Group Project",
+    details:
+      " Is a full e-commerce web application using React.js.This project’s state management was achieved using only ContextAPI to leverage its potentials to the max. A real-world payment gateway (Stripe) was implemented in this project.",
+    skills:
+      "ReactJs - TaillwindCSS- AOS Library - Axios - React Query - React Slick",
+    demo: "https://fresh-cart-seven-gilt.vercel.app/",
+    repo: "https://github.com/ChethanaWijesinghe10/PitCrew",
+  },
+  {
+    name: "FoodOnWay E-commerce Website",
+    details:
+      "Programming Ongoing charity for all dead Muslims. The site is about the Noble Qur’an, interpretation of the Qur’an, hadiths, morning and evening remembrances, and praises.",
+    skills:
+      "React Js - Typescript - ReactQuery- TaillwindCSS - Axios - headlessui-React Hot Toast ",
+    demo: "https://quran-kareem-eta.vercel.app/",
+    repo: "https://github.com/Kalindusu/Foodie-Fusion",
+  },
+  {
+    name: "Chatbot",
+    details:
+      " I've developed a lightweight code editor similar to Visual Studio Code using React,TypeScript, Tailwind CSS, and Redux. The project features local data management withcustom page data and employs UUID for unique ID generation, ensuring robust andorganized state handling..",
+    skills:
+      "ReactJs - Typescript - Reduxjs/toolkit- TaillwindCSS- UUID - React-resizable-panels - React-syntax-highlighter",
+    demo: "https://visual-studio-code-clone-kappa.vercel.app/",
+    repo: "https://github.com/Kalindusu/ChatBot",
+  },
+  {
+    name: "CeylonTours E-Booking Mobile Application",
+    details:
+      " This React and TypeScript project is designed for managing products, featuring functionality to add items with attributes like price, category, image, colors, and description. It uses Headless UI for accessible components, TailwindCSS for modern styling, and UUID for generating unique product identifiers.",
+    skills: "React - TaillwindCSS - React HOOKS - TypeScript - Headlessui",
+    demo: "https://mohammedrady59.github.io/CRUD-SYSTEM-TS/",
+    repo: "https://github.com/Kalindusu/CeylonTours-E-booking",
+  },
+  {
+    name: "Portfolio Website",
+    details:
+      " Developed the company website front-end and currently maintaining it.",
+    skills: "ReactJs - TaillwindCSS - React Slick - AOS Library",
+    demo: "https://foanztopkw.com/",
+    repo: "https://github.com/Kalindusu/Kalindu_Portfolio_new",
+  },
+  // {
+  //   name: "Palestine",
+  //   details:
+  //     "We need to support our brothers in Palestine. By introducing their case against the Zionist occupati...",
+  //   skills: "HTML - CSS - Vanilla JS ",
+  //   demo: "https://mohammedrady59.github.io/Palestine/",
+  //   repo: "https://github.com/MohammedRady59/Palestine",
+  // },
+];
