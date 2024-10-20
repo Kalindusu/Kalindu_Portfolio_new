3eb5fb3fe625d5bae9dbd262bd430b8e73eaa36c
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-20 11:47:33 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ import Model from '../assets/UI/Model';
 const About = () => {
     return ( 
         <div className="border-b border-neutral-500 pb-1">
-            <h1 className="my-20 text-center text-4xl font-bold">About <span className="text-neutral-500">Me</span></h1>
+            <h1 className="my-8 text-center text-4xl font-bold">About <span className="text-neutral-500">Me</span></h1>
 
             <div className="">
             <div className=" pl-10 flex items-center justify-center pb-4 lg:mb-35 "> 
             <motion.div
-                className=" my-4 max-w-2xl py-6 font-light tracking-tight text-lg md:text-xl lg:text-xl text-white"
+                className=" my-2 max-w-2xl py-6 font-light tracking-tight text-lg md:text-xl lg:text-xl text-white"
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: 100 }}
                 transition={{ duration: 0.5 }}
