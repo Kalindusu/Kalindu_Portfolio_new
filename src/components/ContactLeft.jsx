e9d774eb0fb5759204a958aa49eb92e471daf11d
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-20 12:15:27 +0530

    contact left update

--- src/components/ContactLeft.jsx
+++ src/components/ContactLeft.jsx
@@ -5,14 +5,14 @@ const ContactLeft = () => {
     return (
         <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
             <img
-                className="w-full h-64 object-cover rounded-lg mb-2"
+                className="w-full h-70 object-cover rounded-lg mb-2"
                 src={contactImg}
                 alt="contactImg"
             />
             <div className="flex flex-col gap-4">
                 <h3 className="text-3xl font-bold text-white">Kalindu Sudaraka</h3>
                 <p className="text-lg font-normal text-gray-200">
-                    Full Stack Developer / Blogger
+                    Full Stack Developer / Tec Blogger
                 </p>
                 <p className="text-base text-gray-200 flex items-center gap-2">
                     Mobile No: <span className="text-lightText">+94 70 401 2820</span>
